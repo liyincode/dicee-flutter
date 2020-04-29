ab3047f9689889d613b35ecae66fa64c339a8f74
Shuaiyin <shuaiyin.top@foxmai..com>
2020-04-30 07:27:19 +0800

    create widget or wrap widget with 'alt + enter' in android studdio and use FlatButton wrap am amage to press

--- lib/main.dart
+++ lib/main.dart
@@ -18,15 +18,21 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        )
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+                onPressed: () => print('left button on pressed!'),
+                child: Image.asset('images/dice1.png')),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () => print('right button on pressed'),
+                child: Image.asset('images/dice1.png')),
+          )
+        ],
+      ),
     );
   }
 }
